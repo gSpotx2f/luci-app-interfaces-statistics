cd3005264ca531e8c653bb32746049cac37ca9be
gSpot <gspot.x2f@gmail.com>
2021-12-08 20:53:50 +0300

    Support for luci-theme-openwrt-2020

--- README.md
+++ README.md
@@ -7,16 +7,16 @@ OpenWrt >= 19.07.
 
 **OpenWrt >= 21.02:**
 
-    wget --no-check-certificate -O /tmp/luci-app-interfaces-statistics_0.4-2_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-interfaces-statistics_0.4-2_all.ipk
-    opkg install /tmp/luci-app-interfaces-statistics_0.4-2_all.ipk
-    rm /tmp/luci-app-interfaces-statistics_0.4-2_all.ipk
+    wget --no-check-certificate -O /tmp/luci-app-interfaces-statistics_0.4-3_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-app-interfaces-statistics_0.4-3_all.ipk
+    opkg install /tmp/luci-app-interfaces-statistics_0.4-3_all.ipk
+    rm /tmp/luci-app-interfaces-statistics_0.4-3_all.ipk
     /etc/init.d/rpcd restart
 
 i18n-ru:
 
-    wget --no-check-certificate -O /tmp/luci-i18n-interfaces-statistics-ru_0.4-2_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-i18n-interfaces-statistics-ru_0.4-2_all.ipk
-    opkg install /tmp/luci-i18n-interfaces-statistics-ru_0.4-2_all.ipk
-    rm /tmp/luci-i18n-interfaces-statistics-ru_0.4-2_all.ipk
+    wget --no-check-certificate -O /tmp/luci-i18n-interfaces-statistics-ru_0.4-3_all.ipk https://github.com/gSpotx2f/packages-openwrt/raw/master/current/luci-i18n-interfaces-statistics-ru_0.4-3_all.ipk
+    opkg install /tmp/luci-i18n-interfaces-statistics-ru_0.4-3_all.ipk
+    rm /tmp/luci-i18n-interfaces-statistics-ru_0.4-3_all.ipk
 
 **OpenWrt 19.07:**
 
--- htdocs/luci-static/resources/view/interfaces-statistics.js
+++ htdocs/luci-static/resources/view/interfaces-statistics.js
@@ -7,7 +7,7 @@
 const labelColorUp = '#46a546';
 
 return view.extend({
-	ifacesArray: [],
+	ifacesArray  : [],
 
 	callNetDevice: rpc.declare({
 		object: 'network.device',
@@ -17,17 +17,17 @@ return view.extend({
 
 	setIfacesData: function(ifacesData) {
 		for(let iface of this.ifacesArray) {
-			let ifaceState = null;
+			let ifaceState             = null;
 			let ifacesStatisticsObject = null;
 
 			if(ifacesData[iface] !== undefined) {
-				ifaceState = ifacesData[iface].up;
+				ifaceState             = ifacesData[iface].up;
 				ifacesStatisticsObject = ifacesData[iface].statistics;
 			};
 
-			let state = document.querySelector('[data-ifstat="%s_state"]'.format(iface));
+			let state         = document.querySelector('[data-ifstat="%s_state"]'.format(iface));
 			state.textContent = (ifaceState) ? _('Interface is up') : _('Interface is down');
-			state.style = (ifaceState) ? 'background-color:'+ labelColorUp : '';
+			state.style       = (ifaceState) ? 'background-color:'+ labelColorUp : '';
 
 			if(!ifacesStatisticsObject) continue;
 
@@ -94,28 +94,28 @@ return view.extend({
 				this.ifacesArray.sort();
 
 				let ifaceTab = E('div', {
-					'data-tab': tab,
+					'data-tab'      : tab,
 					'data-tab-title': ifaceName,
 				});
 				tabsContainer.append(ifaceTab);
 
-				let ifaceTable = E('div', { 'class': 'table' });
+				let ifaceTable = E('table', { 'class': 'table' });
 
 				if(ifaceMac) {
 					ifaceTable.append(
-						E('div', { 'class': 'tr' }, [
-							E('div', { 'class': 'td left', 'style': 'min-width:33%' },
+						E('tr', { 'class': 'tr' }, [
+							E('td', { 'class': 'td left', 'style': 'min-width:33%' },
 								_('MAC Address') + ':'),
-							E('div', { 'class': 'td left' }, ifaceMac),
+							E('td', { 'class': 'td left' }, ifaceMac),
 						]),
 					);
 				};
 
 				ifaceTable.append(
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'style': 'min-width:33%' },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'style': 'min-width:33%' },
 							_('State') + ':'),
-						E('div', { 'class': 'td left' },
+						E('td', { 'class': 'td left' },
 							E('span', {
 								'data-ifstat': iface + '_state',
 								'class': 'label',
@@ -129,216 +129,216 @@ return view.extend({
 					]),
 				);
 
-				let statTable = E('div', { 'class': 'table' }, [
-					E('div', { 'class': 'tr table-titles' }, [
-						E('div', { 'class': 'th left', 'style': 'min-width:33%' },
+				let statTable = E('table', { 'class': 'table' }, [
+					E('tr', { 'class': 'tr table-titles' }, [
+						E('th', { 'class': 'th left', 'style': 'min-width:33%' },
 							_('Parameter')),
-						E('div', { 'class': 'th left', 'style': 'min-width:33%' },
+						E('th', { 'class': 'th left', 'style': 'min-width:33%' },
 							_('Receive')),
-						E('div', { 'class': 'th left' }, _('Transmit')),
+						E('th', { 'class': 'th left' }, _('Transmit')),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('bytes')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_bytes',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_bytes
 						),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_bytes',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_bytes
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('packets')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_packets',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_packets
 						),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_packets',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_packets
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_errors
 						),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_errors',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_errors
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('dropped')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_dropped',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_dropped
 						),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_dropped',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_dropped
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('compressed')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_compressed',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_compressed
 						),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_compressed',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_compressed
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('fifo errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_fifo_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_fifo_errors
 						),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_fifo_errors',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_fifo_errors
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('multicast')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_multicast',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.multicast
 						),
-						E('div', { 'class': 'td left', 'data-title': _('Transmit') },
+						E('td', { 'class': 'td left', 'data-title': _('Transmit') },
 							'&#160;'),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('crc errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_crc_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_crc_errors
 						),
-						E('div', { 'class': 'td left', 'data-title': _('Transmit') },
+						E('td', { 'class': 'td left', 'data-title': _('Transmit') },
 							'&#160;'),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('frame errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_frame_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_frame_errors
 						),
-						E('div', { 'class': 'td left', 'data-title': _('Transmit') },
+						E('td', { 'class': 'td left', 'data-title': _('Transmit') },
 							'&#160;'),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('length errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_length_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_length_errors
 						),
-						E('div', { 'class': 'td left', 'data-title': _('Transmit') },
+						E('td', { 'class': 'td left', 'data-title': _('Transmit') },
 							'&#160;'),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('missed errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_missed_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_missed_errors
 						),
-						E('div', { 'class': 'td left', 'data-title': _('Transmit') },
+						E('td', { 'class': 'td left', 'data-title': _('Transmit') },
 							'&#160;'),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('over errors')),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_rx_over_errors',
-							'data-title': _('Receive') },
+							'data-title' : _('Receive') },
 							ifacesStatisticsObject.rx_over_errors
 						),
-						E('div', { 'class': 'td left', 'data-title': _('Transmit') },
+						E('td', { 'class': 'td left', 'data-title': _('Transmit') },
 							'&#160;'),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('collisions')),
-						E('div', { 'class': 'td left', 'data-title': _('Receive') },
+						E('td', { 'class': 'td left', 'data-title': _('Receive') },
 							'&#160;'),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_collisions',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.collisions
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('carrier errors')),
-						E('div', { 'class': 'td left', 'data-title': _('Receive') },
+						E('td', { 'class': 'td left', 'data-title': _('Receive') },
 							'&#160;'),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_carrier_errors',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_carrier_errors
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('heartbeat errors')),
-						E('div', { 'class': 'td left', 'data-title': _('Receive') },
+						E('td', { 'class': 'td left', 'data-title': _('Receive') },
 							'&#160;'),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_heartbeat_errors',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_heartbeat_errors
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('aborted errors')),
-						E('div', { 'class': 'td left', 'data-title': _('Receive') },
+						E('td', { 'class': 'td left', 'data-title': _('Receive') },
 							'&#160;'),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_aborted_errors',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_aborted_errors
 						),
 					]),
-					E('div', { 'class': 'tr' }, [
-						E('div', { 'class': 'td left', 'data-title': _('Parameter') },
+					E('tr', { 'class': 'tr' }, [
+						E('td', { 'class': 'td left', 'data-title': _('Parameter') },
 							_('window errors')),
-						E('div', { 'class': 'td left', 'data-title': _('Receive') },
+						E('td', { 'class': 'td left', 'data-title': _('Receive') },
 							'&#160;'),
-						E('div', { 'class': 'td left',
+						E('td', { 'class': 'td left',
 							'data-ifstat': ifaceName + '_tx_window_errors',
-							'data-title': _('Transmit') },
+							'data-title' : _('Transmit') },
 							ifacesStatisticsObject.tx_window_errors
 						),
 					]),
